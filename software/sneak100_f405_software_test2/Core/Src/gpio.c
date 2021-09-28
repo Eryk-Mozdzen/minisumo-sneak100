fff8dcbe5eb3f735e740dd17b825a7989875ca79
Eryk Mozdzen <ermozdzen@gmail.com>
2021-09-29 01:19:40 +0200

    uController routes in progress

--- software/sneak100_f405_software_test2/Core/Src/gpio.c
+++ software/sneak100_f405_software_test2/Core/Src/gpio.c
@@ -50,7 +50,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, BLUETOOTH_PWR_Pin|BLUETOOTH_EN_Pin|USER_LED_GREEN_Pin|USER_LED_YELLOW_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, BLUETOOTH_PWR_Pin|BLUETOOTH_EN_Pin|USER_LED_YELLOW_Pin|USER_LED_GREEN_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = RECEIVER_OUT_Pin;
@@ -78,7 +78,7 @@ void MX_GPIO_Init(void)
   HAL_GPIO_Init(BLUETOOTH_ST_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PBPin PBPin PBPin */
-  GPIO_InitStruct.Pin = BLUETOOTH_EN_Pin|USER_LED_GREEN_Pin|USER_LED_YELLOW_Pin;
+  GPIO_InitStruct.Pin = BLUETOOTH_EN_Pin|USER_LED_YELLOW_Pin|USER_LED_GREEN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
