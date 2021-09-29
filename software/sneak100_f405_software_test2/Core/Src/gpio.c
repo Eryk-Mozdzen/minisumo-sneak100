79f4627fd3f5c586da8abc55aa280a4129180272
Eryk Mozdzen <ermozdzen@gmail.com>
2021-09-29 15:35:37 +0200

    uController routes finish

--- software/sneak100_f405_software_test2/Core/Src/gpio.c
+++ software/sneak100_f405_software_test2/Core/Src/gpio.c
@@ -50,7 +50,10 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, BLUETOOTH_PWR_Pin|BLUETOOTH_EN_Pin|USER_LED_YELLOW_Pin|USER_LED_GREEN_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(BLUETOOTH_PWR_GPIO_Port, BLUETOOTH_PWR_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, USER_LED_YELLOW_Pin|USER_LED_GREEN_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = RECEIVER_OUT_Pin;
@@ -58,8 +61,16 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(RECEIVER_OUT_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PAPin PAPin PAPin PAPin */
-  GPIO_InitStruct.Pin = PROXIMITY_LL_Pin|PROXIMITY_FL_Pin|PROXIMITY_FR_Pin|PROXIMITY_RR_Pin;
+  /*Configure GPIO pins : PCPin PCPin PCPin PCPin
+                           PCPin */
+  GPIO_InitStruct.Pin = PROXIMITY_RR_Pin|PROXIMITY_FR_Pin|USER_BUTTON_L_Pin|USER_BUTTON_C_Pin
+                          |USER_BUTTON_R_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PAPin PAPin */
+  GPIO_InitStruct.Pin = BLUETOOTH_ST_Pin|BLUETOOTH_EN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
@@ -71,24 +82,18 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(BLUETOOTH_PWR_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PtPin */
-  GPIO_InitStruct.Pin = BLUETOOTH_ST_Pin;
+  /*Configure GPIO pins : PBPin PBPin */
+  GPIO_InitStruct.Pin = PROXIMITY_FL_Pin|PROXIMITY_LL_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(BLUETOOTH_ST_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PBPin PBPin PBPin */
-  GPIO_InitStruct.Pin = BLUETOOTH_EN_Pin|USER_LED_YELLOW_Pin|USER_LED_GREEN_Pin;
+  /*Configure GPIO pins : PBPin PBPin */
+  GPIO_InitStruct.Pin = USER_LED_YELLOW_Pin|USER_LED_GREEN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PCPin PCPin PCPin */
-  GPIO_InitStruct.Pin = USER_BUTTON_L_Pin|USER_BUTTON_C_Pin|USER_BUTTON_R_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
 }
 
