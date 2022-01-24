0d770a6989f6bb98127c100606109c5a333595c0
Eryk Możdżeń <ermozdzen@gmail.com>
2022-01-24 21:34:34 +0100

    2nd board test

---
+++ software/sneak100_f405_software_test2/Debug/Core/Src/stm32f4xx_hal_timebase_tim.d
@@ -0,0 +1,101 @@
+Core/Src/stm32f4xx_hal_timebase_tim.o: \
+ ../Core/Src/stm32f4xx_hal_timebase_tim.c \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ ../Core/Inc/stm32f4xx_hal_conf.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f405xx.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/core_cm4.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_adc.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+../Core/Inc/stm32f4xx_hal_conf.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f405xx.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_version.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/mpu_armv7.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_adc.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
--- software/sneak100_f405_software_test2/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.d
+++ software/sneak100_f405_software_test2/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.d
@@ -1,101 +1,101 @@
 Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.o: \
  ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_adc_ex.c \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
  ../Core/Inc/stm32f4xx_hal_conf.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
- ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
- ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f405xx.h \
- ../Drivers/CMSIS/Include/core_cm4.h \
- ../Drivers/CMSIS/Include/cmsis_version.h \
- ../Drivers/CMSIS/Include/cmsis_compiler.h \
- ../Drivers/CMSIS/Include/cmsis_gcc.h \
- ../Drivers/CMSIS/Include/mpu_armv7.h \
- ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_adc.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f405xx.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/core_cm4.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_adc.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ /home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 
 ../Core/Inc/stm32f4xx_hal_conf.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
 
-../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
 
-../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f405xx.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f405xx.h:
 
-../Drivers/CMSIS/Include/core_cm4.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/core_cm4.h:
 
-../Drivers/CMSIS/Include/cmsis_version.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_version.h:
 
-../Drivers/CMSIS/Include/cmsis_compiler.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_compiler.h:
 
-../Drivers/CMSIS/Include/cmsis_gcc.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/cmsis_gcc.h:
 
-../Drivers/CMSIS/Include/mpu_armv7.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Include/mpu_armv7.h:
 
-../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_adc.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_adc.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_adc_ex.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
 
-../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
+/home/emozdzen/Desktop/minisumo-sneak100/software/sneak100_f405_software_test2/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
