9b6ebe429346432c77cd8428a26c709ce88cc852
Eryk Możdżeń <ermozdzen@gmail.com>
2022-01-27 18:11:26 +0100

    better main menu

--- software/boards_layout/main_board_v1.1/Inc/sneak100_display.h
+++ software/boards_layout/main_board_v1.1/Inc/sneak100_display.h
@@ -22,13 +22,27 @@
 #define DISPLAY_LINE_3	32
 #define DISPLAY_LINE_4	42
 
+#define GUI_MENU_ROW_0	0
+#define GUI_MENU_ROW_1	13
+#define GUI_MENU_ROW_2	26
+#define GUI_MENU_ROW_3	39
+
+#define GUI_MENU_COL_0	0
+#define GUI_MENU_COL_1	64
+
 typedef enum {
-	GUI_STATE_DESKTOP,
+	GUI_STATE_MENU,
+
 	GUI_STATE_VIEW_MOTORS,
 	GUI_STATE_VIEW_LINE,
 	GUI_STATE_VIEW_PROXIMITY,
 	GUI_STATE_VIEW_OTHERS,
+
 	GUI_STATE_SETTINGS,
+	GUI_STATE_TEST,
+	GUI_STATE_FIGHT,
+
+	GUI_STATE_INFO,
 	GUI_STATE_CREDITS
 } GUI_States_EnumtypeDef;
 
@@ -65,6 +79,8 @@ typedef struct {
 	GUI_ButtonState_StructTypeDef buttons[3];
 	Display_StructTypeDef display;
 
+	uint8_t menu_selected;
+
 	RobotState_StructTypeDef data;
 } Sneak100_GUI_StructTypeDef;
 
@@ -72,5 +88,16 @@ extern Sneak100_GUI_StructTypeDef gui;
 
 void SNEAK100_Display_Init();
 void SNEAK100_Display_Update();
+
+uint8_t __Button_L_ClickEvent(void *);
+uint8_t __Button_C_ClickEvent(void *);
+uint8_t __Button_R_ClickEvent(void *);
+
+uint8_t __GUI_Menu_View_SelectEvent(void *);
+uint8_t __GUI_Menu_Settings_SelectEvent(void *);
+uint8_t __GUI_Menu_Test_SelectEvent(void *);
+uint8_t __GUI_Menu_Fight_SelectEvent(void *);
+uint8_t __GUI_Menu_Info_SelectEvent(void *);
+uint8_t __GUI_Menu_Credits_SelectEvent(void *);
 
 #endif /* SNEAK100_HARDWARE_INC_SNEAK100_DISPLAY_H_ */
