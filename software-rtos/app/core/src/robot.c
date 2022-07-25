6176090661d6520c83d5211b22ec77b434c26dc7
Eryk Możdżeń <ermozdzen@gmail.com>
2022-07-25 02:17:17 +0200

    robot state machine

---
+++ software-rtos/app/core/src/robot.c
@@ -0,0 +1,154 @@
+#include "robot.h"
+
+static rc5_message_t rc5_msg;
+static FiniteStateMachine_t fsm;
+
+static struct {
+	uint8_t rc5_program_cmd : 1;
+	uint8_t rc5_start_cmd : 1;
+	uint8_t rc5_stop_cmd : 1;
+	uint8_t flash_led_state : 1;
+	uint8_t unused : 4;
+} flags = {0};
+
+static struct {
+	robot_state_t sm_state;
+	uint16_t stop_cmd;
+	uint16_t start_cmd;
+} eeprom_data;
+
+static void handle_message() {
+	if(rc5_msg.address==0x0B) {
+		// programming command
+
+		flags.rc5_program_cmd = 1;
+
+		eeprom_data.stop_cmd = rc5_msg.command & 0xFE;
+		eeprom_data.start_cmd = (rc5_msg.command & 0xFE) + 1;
+
+		eeprom_write(ROBOT_DATA_EEPROM_PAGE, ROBOT_DATA_EEPROM_OFFSET, &eeprom_data, sizeof(eeprom_data));
+
+	} else if(rc5_msg.command==eeprom_data.stop_cmd) {
+		// stop command
+
+		flags.rc5_stop_cmd = 1;
+
+	} else if(rc5_msg.command==eeprom_data.start_cmd) {
+		// start command
+
+		flags.rc5_start_cmd = 1;
+
+	}
+}
+
+static void loop(void *param) {
+	(void)param;
+
+	static_assert(sizeof(eeprom_data)<=EEPROM_PAGE_SIZE);
+	eeprom_read(ROBOT_DATA_EEPROM_PAGE, ROBOT_DATA_EEPROM_OFFSET, &eeprom_data, sizeof(eeprom_data));
+
+	switch(eeprom_data.sm_state) {
+		case ROBOT_STATE_READY:
+		case ROBOT_STATE_PROGRAM:
+		case ROBOT_STATE_RUN:
+		case ROBOT_STATE_STOP: break;
+		default: eeprom_data.sm_state = ROBOT_STATE_READY; break;
+	}
+
+	FiniteStateMachine_Start(&fsm, eeprom_data.sm_state);
+
+	while(1) {
+		if(rc5_get_message(&rc5_msg)) {
+			handle_message();
+		}
+
+		FiniteStateMachine_Update(&fsm);
+		FiniteStateMachine_Execute(&fsm);
+
+		flags.rc5_program_cmd = 0;
+		flags.rc5_stop_cmd = 0;
+		flags.rc5_start_cmd = 0;
+
+		vTaskDelay(1000/ROBOT_LOOP_FREQ);
+	}
+}
+
+static uint8_t get_program(void *buffer) {
+	(void)buffer;
+	return flags.rc5_program_cmd;
+}
+
+static uint8_t get_start(void *buffer) {
+	(void)buffer;
+	return flags.rc5_start_cmd;
+}
+
+static uint8_t get_stop(void *buffer) {
+	(void)buffer;
+	return flags.rc5_stop_cmd;
+}
+
+static void ready_enter(void *buffer) {
+	(void)buffer;
+
+	eeprom_data.sm_state = ROBOT_STATE_READY;
+	eeprom_write(ROBOT_DATA_EEPROM_PAGE, ROBOT_DATA_EEPROM_OFFSET, &eeprom_data, sizeof(eeprom_data));
+}
+
+static void program_enter(void *buffer) {
+	(void)buffer;
+
+	led_set_yellow(1);
+	vTaskDelay(250);
+	led_set_yellow(0);
+	vTaskDelay(250);
+
+	led_set_yellow(1);
+	vTaskDelay(250);
+	led_set_yellow(0);
+	vTaskDelay(250);
+}
+
+static void run_enter(void *buffer) {
+	(void)buffer;
+
+	led_set_yellow(1);
+
+	eeprom_data.sm_state = ROBOT_STATE_RUN;
+	eeprom_write(ROBOT_DATA_EEPROM_PAGE, ROBOT_DATA_EEPROM_OFFSET, &eeprom_data, sizeof(eeprom_data));
+}
+
+static void stop_enter(void *buffer) {
+	(void)buffer;
+
+	eeprom_data.sm_state = ROBOT_STATE_READY;
+	eeprom_write(ROBOT_DATA_EEPROM_PAGE, ROBOT_DATA_EEPROM_OFFSET, &eeprom_data, sizeof(eeprom_data));
+}
+
+static void stop_execute(void *buffer) {
+	(void)buffer;
+
+	led_set_yellow(flags.flash_led_state);
+	
+	flags.flash_led_state = !flags.flash_led_state;
+
+	vTaskDelay(500);
+}
+
+void robot_init() {
+
+	FiniteStateMachine_Init(&fsm, NULL);
+
+	FiniteStateMachine_DefineState(&fsm, ROBOT_STATE_READY,		ready_enter,	NULL,			NULL);
+	FiniteStateMachine_DefineState(&fsm, ROBOT_STATE_PROGRAM,	program_enter,	NULL,			NULL);
+	FiniteStateMachine_DefineState(&fsm, ROBOT_STATE_RUN,		run_enter,		NULL,			NULL);
+	FiniteStateMachine_DefineState(&fsm, ROBOT_STATE_STOP,		stop_enter,		stop_execute,	NULL);
+
+	FiniteStateMachine_DefineTransition(&fsm, ROBOT_STATE_READY,	ROBOT_STATE_PROGRAM,	0, NULL, get_program);
+	FiniteStateMachine_DefineTransition(&fsm, ROBOT_STATE_PROGRAM,	ROBOT_STATE_READY,		0, NULL, NULL);
+	FiniteStateMachine_DefineTransition(&fsm, ROBOT_STATE_READY,	ROBOT_STATE_RUN,		0, NULL, get_start);
+	FiniteStateMachine_DefineTransition(&fsm, ROBOT_STATE_RUN,		ROBOT_STATE_STOP,		0, NULL, get_stop);
+	FiniteStateMachine_DefineTransition(&fsm, ROBOT_STATE_RUN,		ROBOT_STATE_PROGRAM,	0, NULL, get_program);
+
+	xTaskCreate(loop, "robot loop", 256, NULL, 4, NULL);
+}
